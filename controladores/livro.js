d9acf525791e4f30a891963a88770a07468f0627
Sarah Kédma <sarahkedmacontact@gmail.com>
2024-12-05 06:55:22 -0300

    first commit

---
+++ controladores/livro.js
@@ -0,0 +1,16 @@
+const fs = require("fs")
+
+function getLivros(req, res) {
+    try {
+        const livros = JSON.parse(fs.readFileSync("livros.json"))
+        res.send(livros)
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    } 
+}
+
+
+module.exports = {
+    getLivros
+}
