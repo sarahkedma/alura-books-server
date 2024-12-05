5b3a96d90bfcf668ab7a1ca7eacc14a9be663207
Sarah Kédma <sarahkedmacontact@gmail.com>
2024-12-05 16:11:10 -0300

    cors

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express")
+const cors = require("cors")
 const rotaLivro = require('./rotas/livro')
 const app = express()
 app.use(express.json())
+app.use(cors({origin: "*"}))
+
 app.use('/livros', rotaLivro)
 
 const port = 8000
