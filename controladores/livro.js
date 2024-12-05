1820df30ed76e180cd879c3ebfcc7dc0f8b8c6d0
Sarah Kédma <sarahkedmacontact@gmail.com>
2024-12-05 09:52:53 -0300

    ah sim

--- controladores/livro.js
+++ controladores/livro.js
@@ -1,4 +1,5 @@
-const { getTodosLivros } = require("../servicos/livro")
+const { getTodosLivros, getLivroPorId } = require("../servicos/livro");
+
 const fs = require("fs")
 
 function getLivros(req, res) {
@@ -11,7 +12,17 @@ function getLivros(req, res) {
     } 
 }
 
-
+function getLivro(req, res) {
+    try {
+        const id = req.params.id
+        const livro =  getLivroPorId(id)
+        res.send(livro)
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    } 
+}
 module.exports = {
+    getLivro, 
     getLivros
 }
--- servicos/livro.js
+++ servicos/livro.js
@@ -4,6 +4,15 @@ function getTodosLivros() {
     return JSON.parse( fs.readFileSync("livros.json") )
 }
 
+function getLivroPorId(id) {
+    const livro = JSON.parse(fs.readFileSync("livros.json"))
+
+const livroFiltrado = livro.filter( livro => livro.id === id) [0]
+    return livroFiltrado
+}
+
+
 module.exports =  {
-    getTodosLivros
+    getTodosLivros,
+    getLivroPorId
 }
